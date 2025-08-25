1e451b3d63e81b197900055f22c072296f61bcf7
zahidrahimoon <zahidrahimoon22@gmail.com>
2025-08-24 21:57:32 -0400

    go lang starter

---
+++ go/basic.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(1)
+	fmt.Println("Hello")
+	fmt.Println(true)
+	fmt.Println(3.14)
+	fmt.Println(3.14/2)
+	fmt.Println("Goodbye")
+}
